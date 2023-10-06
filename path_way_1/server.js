f426fd5b6d2754bfec354df52657151a7b7c1513
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-10-06 18:43:18 +0100

    changed DB_SSL

--- path_way_1/server.js
+++ path_way_1/server.js
@@ -25,7 +25,7 @@ const db = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 db.connect(function (err) {
