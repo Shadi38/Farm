dc758c4c00fde73c4b3600cad342b3abc6cf3f5d
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-10-19 13:38:50 +0100

    changed header

--- path_way_1/src/Header.js
+++ path_way_1/src/Header.js
@@ -9,16 +9,14 @@ function Header() {
           <h1>Welcome to our Farm</h1>
         </header>
 
-        <h2 style={{ color: "rgb(28, 27, 27)" }}>
+        <p style={{ color: "rgb(28, 27, 27)" }}>
           Our farm needs volunteers to feed animals.
-          <br />
           In "Sessions" section you can register.
-          <br />
           In the "Sessions/Volunteers" section, you can view all the sessions
           available for volunteering on our farm. This section also displays the
           sessions that have already been booked by volunteers and the names of
           the volunteers who have made those bookings.
-        </h2>
+        </p>
       </div>
     </>
   );  
